68399d315ab13602f07a96d94892b55b7461884e
sedaten <sedaten06@gmail.com>
2020-09-11 22:20:34 +0200

    19. gun ders sonu

---
+++ src/test/java/com/techproed/tests/FacebookLoginTest.java
@@ -0,0 +1,21 @@
+package com.techproed.tests;
+
+import com.sun.deploy.cache.BaseLocalApplicationProperties;
+import com.techproed.pages.FacebookLoginPage;
+import com.techproed.utilities.ConfigurationReader;
+import com.techproed.utilities.TestBase;
+import org.testng.annotations.Test;
+
+public class FacebookLoginTest extends TestBase {
+
+    @Test
+    public void girisTesti(){
+        driver.get("http://facebook.com");
+        FacebookLoginPage loginPage = new FacebookLoginPage(driver);
+        loginPage.emailKutusu.sendKeys(ConfigurationReader.getProperty("fb_email"));
+        loginPage.sifreKutusu.sendKeys(ConfigurationReader.getProperty("fb_pass"));
+    }
+
+
+
+}
---
+++ src/test/java/smoketests/FacebookLoginTest.java
@@ -0,0 +1,6 @@
+package smoketests;
+
+public class FacebookLoginTest {
+
+
+}
